4312311a48570d512bf54fbe78216b9f34ac9a6e
Nate Festinger <nate.festinger@gmail.com>
2017-09-20 12:56:44 -0700

    Add backend auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   root 'static_pages#root'
+
+  namespace :api, defaults: { format: :json } do
+    resource :session, only: [:create, :destroy] # login, logout
+    resources :users, only: [:show, :create, :destroy] # sign up, delete account
+  end
 end
